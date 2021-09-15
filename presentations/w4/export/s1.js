69fa703eed2f00a28931d41e6a82adb31a705ee6
lucidbard <lucidbard@users.noreply.github.com>
2021-09-15 13:00:28 -0400

    September 15, 2021 1:00 PM

--- presentations/w4/export/s1.js
+++ presentations/w4/export/s1.js
@@ -1,25 +1,27 @@
-let circleDraw, squareDraw
+let sketch1 = function (p) {
+  let circleDraw, squareDraw
 
-function setup() {
-  var canvas = createCanvas(400, 100);
-  canvas.parent('sketch-holder');
-  circleDraw = createButton("drawCircle(30,30)");
-  circleDraw.mousePressed(() => drawCircle(30, 30))
-  circleDraw.parent('sketch-holder');
-  squareDraw = createButton("randomRectangle()");
-  squareDraw.mousePressed(randomRectangle);
-  squareDraw.parent('sketch-holder');
-  background(50);
-  noStroke();
-}
-function draw() {
-}
-function randomRectangle() {
-  fill(random(255));
-  rect(random() * width, random() * height,
-    random(width / 2), random(width / 2));
-}
+  function setup() {
+    var canvas = createCanvas(400, 100);
+    canvas.parent('sketch-holder');
+    circleDraw = createButton("drawCircle(30,30)");
+    circleDraw.mousePressed(() => drawCircle(30, 30))
+    circleDraw.parent('sketch-holder');
+    squareDraw = createButton("randomRectangle()");
+    squareDraw.mousePressed(randomRectangle);
+    squareDraw.parent('sketch-holder');
+    background(50);
+    noStroke();
+  }
+  function draw() {
+  }
+  function randomRectangle() {
+    fill(random(255));
+    rect(random() * width, random() * height,
+      random(width / 2), random(width / 2));
+  }
 
-function drawCircle(x, y) {
-  circle(x, y, 30)
+  function drawCircle(x, y) {
+    circle(x, y, 30)
+  }
 }
