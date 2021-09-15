55cc180ea599ab8db48b55fd2b0a4362b7216eec
lucidbard <lucidbard@users.noreply.github.com>
2021-09-15 13:02:34 -0400

    September 15, 2021 1:02 PM

---
+++ presentations/w4/s1 copy.js
@@ -0,0 +1,28 @@
+let sketch1 = function (p) {
+  let circleDraw, squareDraw
+
+  p.setup = function () {
+    var canvas = p.createCanvas(400, 100);
+    canvas.parent('sketch-holder');
+    circleDraw = p.createButton("drawCircle(30,30)");
+    circleDraw.mousePressed(() => drawCircle(30, 30))
+    circleDraw.parent('sketch-holder');
+    squareDraw = p.createButton("randomRectangle()");
+    squareDraw.mousePressed(randomRectangle);
+    squareDraw.parent('sketch-holder');
+    p.background(50);
+    p.noStroke();
+  }
+  p.draw() = function () {
+  }
+
+  let randomRectangle = function () {
+    p.fill(random(255));
+    p.rect(random() * width, random() * height,
+      random(width / 2), random(width / 2));
+  }
+
+  function drawCircle(x, y) {
+    p.circle(x, y, 30)
+  }
+}
